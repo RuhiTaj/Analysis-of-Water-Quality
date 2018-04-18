e59a58f36a6591365f37227817e652bcdcc420f6
Unknown <ranjithm9634@gmail.com>
2018-04-18 16:41:07 +0530

    Updated SVM

--- svm.py
+++ svm.py
@@ -1,12 +1,33 @@
+import numpy
 from sklearn import svm
-# X and y have to be taken as input 
-X = [[0, 0], [1, 1]]
-y = [0, 1]
+from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA
+from pandas import read_csv
+# load data
+url = '/home/ranjith/Analysis-of-Water-Quality-master/dataset.csv'
+
+names = ['MG','PH','K','NITRATE','SULPHATE','CARBONATE','CHLORIDE','FLUORIDE', 'Water Quality']
+dataframe = read_csv(url, names=names)
+array = dataframe.values
+
+X_train = array[0:18][:,0:8]
+y_train = array[0:18][:,8]
+#X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.4, random_state=0)
+X_test = array[18:23][:,0:8]
+y_test = array[18:23][:,8]
+
+
+
 
 #SVM
-clf = svm.SVC()
-clf.fit(X, y) 
+clf = svm.SVC(kernel="rbf", C=100, gamma=0.1)
+clf.fit(X_train, y_train) 
 
 #Using this we can print output
-#print clf.predict([[1, 1]])
-#Out put is 1
+
+predicted_labels = clf.predict(X_test)
+print predicted_labels
+print"\n\n"
+print('{:.2%}\n'.format(accuracy_score(y_test, predicted_labels)))
+
+#Output is 1
